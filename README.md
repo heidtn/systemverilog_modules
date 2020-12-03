7ff16494e0b7dea10c778bf3520efe32fa1a0c57
Nathan <nathanielheidt@gmail.com>
2020-12-03 15:35:16 -0800

    Added gitignore and actual test

---
+++ README.md
@@ -0,0 +1,2 @@
+### FPGA Kalman
+This is an FPGA implementation of a kalman filter in systemverilog intended to run on an ICE40 board.  I am not currently a good HDL designer, so this isn't perfect.  Use at your own risk.
--- tests/test_mat_mul.py
+++ tests/test_mat_mul.py
@@ -21,18 +21,16 @@ async def test_mat_mul_simple(dut):
     for i in range(2):
         #val = random.randint(0, 1)
         #dut.d <= val  # Assign the random value val to the input port d
+        in_arr = [1.5, 2, 3, 4, 5, 6, 7, 8, 9]
         dut.i_mat1 <= arr_to_fixed([1.0, 0, 0, 0, 1, 0, 0, 0, 1], 8)
-        dut.i_mat2 <= arr_to_fixed([1.5, 2, 3, 4, 5, 6, 7, 8, 9], 8)
+        dut.i_mat2 <= arr_to_fixed(in_arr, 8)
         await Timer(100)
         #dut._log.info(f"matcalc: {dut.mat_calc.value}")
         dut._log.info(f"imat is {arr_to_float(dut.i_mat1.value, 8)} imat2 is {arr_to_float(dut.i_mat2.value, 8)}")
         dut._log.info(f"o_done: {dut.o_done.value}")
         dut._log.info(f"omat is {arr_to_float(dut.o_mat.value, 8)}")
         await RisingEdge(dut.i_clk)
-        #dut._log.info(f"matcalc: {dut.mat_calc.value}")
-        dut._log.info(f"omat is {arr_to_float(dut.o_mat.value, 8)}")
-        dut._log.info(f"o_done: {dut.o_done.value}")
-        await RisingEdge(dut.i_clk)
+        assert dut.o_mat.value == arr_to_fixed(in_arr, 8)
 
         #mul.i_mat2 <= [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
         #await FallingEdge(mul.i_clk)
